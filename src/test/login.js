ec7a07fc1c0024c97f996539eb80c749f53edf5f
ux-builder <sdh485@gmail.com>
2024-03-04 09:29:21 +0900

    update

--- src/test/login.js
+++ src/test/login.js
@@ -4,6 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
   loginButton.addEventListener('click', function() {
       // 중계 서버의 로그인 URL로 리다이렉트
       // 'client-id'를 고객사 서비스에 맞게 변경해야 합니다.
-      window.location.href = 'https://okrbiz.com:8100/login/client-id';
+      window.location.href = 'https://okrbiz.com:8100/auth/login/client-id';
   });
 });
