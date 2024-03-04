354896bb562828c22bc154eb0e0828463bacbd9e
ux-builder <sdh485@gmail.com>
2024-03-04 10:03:24 +0900

    update

--- src/test/login.js
+++ src/test/login.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var loginButton = document.getElementById('login-button');
+document.addEventListener("DOMContentLoaded", function () {
+  var loginButton = document.getElementById("login-button");
 
-  loginButton.addEventListener('click', function() {
-      // 중계 서버의 로그인 URL로 리다이렉트
-      // 'client-id'를 고객사 서비스에 맞게 변경해야 합니다.
-      window.location.href = 'https://okrbiz.com:8100/auth/login/client-id';
+  loginButton.addEventListener("click", function () {
+    // 중계 서버의 로그인 URL로 리다이렉트
+    // 'client-id'를 고객사 서비스에 맞게 변경해야 합니다.
+    window.location.href = "https://okrbiz.com:8100/auth/login/5240corp";
   });
 });
