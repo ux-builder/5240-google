4abe90b94c08ceb959455841f898af5b0d90fa96
syb <dpqls0625@naver.com>
2024-03-20 22:35:28 +0000

    update

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const axios = require("axios"); // axios library
+const jwt = require('jsonwebtoken');
+const forge = require('node-forge');
 const Keycloak = require("keycloak-connect");
 const path = require('path');
-const jwtDecode = require('jwt-decode');
 
 const redirectUri = process.env.REDIRECT_URI;
 const mainDomain = process.env.MAIN_DOMAIN;
+const secretKey = process.env.SECRET_KEY;
 
 // Keycloak configuration load function
 function loadKeycloakConfig(clientId) {
@@ -39,6 +41,23 @@ function randomString(length = 32) {
   return result;
 }
 
+function aesEncrypt(text, key) {
+  const cipher = forge.cipher.createCipher('AES-CBC', forge.util.createBuffer(key.slice(0, 16)));
+  cipher.start({iv: key.slice(0, 16)});
+  cipher.update(forge.util.createBuffer(text, 'utf8'));
+  cipher.finish();
+  const encrypted = cipher.output.getBytes();
+  return forge.util.encode64(encrypted);
+}
+
+function aesDecrypt(encryptedText, key) {
+  const decipher = forge.cipher.createDecipher('AES-CBC', forge.util.createBuffer(key.slice(0, 16)));
+  decipher.start({iv: key.slice(0, 16)});
+  decipher.update(forge.util.createBuffer(forge.util.decode64(encryptedText), 'raw'));
+  decipher.finish();
+  return decipher.output.toString('utf8');
+}
+
 router.get("/login/:clientId", (req, res) => {
   console.log(`redirectUri: ${redirectUri}`);
   const clientId = req.params.clientId;
@@ -54,11 +73,23 @@ router.get("/login/:clientId", (req, res) => {
 
     console.log(`Keycloak config realm: ${keycloak.config.realmUrl}`);
 
+    // IdP로 Google 지정
+    const idpHint = 'google'; // Google IdP의 이름
+
     // URL 매개변수를 각 줄로 분리하여 가독성 향상
+/*    const loginUrl = `${realmUrl}/protocol/openid-connect/auth` +
+                     `?client_id=${encodeURIComponent(clientId)}` +
+                     `&redirect_uri=${encodeURIComponent(`${redirectUri}/callback?client_id=${clientId}`)}` +
+                     `&response_type=${responseType}` + 
+                     `&scope=openid`;
+*/
     const loginUrl = `${realmUrl}/protocol/openid-connect/auth` +
                      `?client_id=${encodeURIComponent(clientId)}` +
                      `&redirect_uri=${encodeURIComponent(`${redirectUri}/callback?client_id=${clientId}`)}` +
-                     `&response_type=${responseType}`;
+                     `&response_type=${responseType}` + 
+                     `&scope=openid` +
+                     `&kc_idp_hint=${idpHint}`; // Google 인증을 위한 IdP 힌트 추가
+
     console.log(`Redirecting to Keycloak login URL for client: ${loginUrl}`); // Log redirection
 
     // Redirect to the created URL
@@ -96,6 +127,7 @@ router.get("/callback", async (req, res) => {
     params.append('client_secret', keycloakConfig.config.secret);
     params.append('redirect_uri', tokenRequestRedirectUri);
     params.append('code', code);
+    params.append('scope','openid')
     
     const response = await axios({
       method: 'post',
@@ -105,21 +137,53 @@ router.get("/callback", async (req, res) => {
     });
 
     console.log(`data : -----------------------------------`);
-    console.log(response.data);
-
-
+ //   console.log(response.data);
 
     const { access_token, refresh_token, id_token } = response.data;
     console.log(`Token received successfully for client: ${clientId}`); // Log success
 
+    //console.log(`id_token : ${id_token}`);
     //const { id_token } = response.data;
-    //const decodedToken = jwtDecode(id_token);
-    //console.log(decodedToken); // 이 객체에서 필요한 정보를 찾습니다.
-    console.log(`id_token : ${id_token}`);
+    const decodedToken = jwt.decode(id_token);
+    console.log(decodedToken.email); // 이 객체에서 필요한 정보를 찾습니다.
+
+    const encryptedUserMailId = aesEncrypt(decodedToken.email, secretKey);
+
+    const decryptedUserMailId = aesDecrypt(encryptedUserMailId, secretKey);
+
+    console.log('Encrypted:', encryptedUserMailId);
+    console.log('Decrypted:', decryptedUserMailId);
     
-    const redirectUrl = `https://${clientId}.${mainDomain}/googleSsologinUser.do#access_token=${access_token}&refresh_token=${refresh_token}&userMailId=${id_token}`;
+    const serviceAreaId = 1;
+    const today = new Date().toISOString().slice(0, 10).replace(/-/g, ''); // 현재 날짜를 yyyymmdd 형식으로 변환
+    
+    const interfaceKey = (serviceAreaId * parseInt(today)) 
+      + (serviceAreaId * serviceAreaId) 
+      + (parseInt(today) * parseInt(today));
+
+    const encryptedInterfaceKey = aesEncrypt(interfaceKey, secretKey);  
+/*
+    // 다른 서버로 POST 요청 보내기
+    const targetUrl = `https://${clientId}.${mainDomain}/googleSsologinUser.do`;
+    const postData = {
+      userMailId: encryptedUserMailId,
+      googleToken: access_token,
+      interfaceKey: encryptedInterfaceKey
+    };
+
+    await axios.post(targetUrl, postData);
+
+    // 응답 처리
+    //res.send(redirectUrl.data);
+*/
+    const redirectUrl = `https://${clientId}.${mainDomain}/googleSsologinUser.do` +
+                        `?userMailId=${encodeURIComponent(encryptedUserMailId)}` + 
+                        `&googleToken=${encodeURIComponent(access_token)}` +
+                        `&interfaceKey=${encodeURIComponent(encryptedInterfaceKey)}`;
+
     console.log(`redirectUrl : ${redirectUrl}`);
     res.redirect(redirectUrl);
+
   } catch (error) {
     console.error("Error requesting token from Keycloak:", error);
     res.status(500).send("Error requesting token from Keycloak");
